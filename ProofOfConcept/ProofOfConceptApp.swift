4116f65ac4e7acf239a554add7ae1fd469ef4d0f
Alexandru Blaga <alexandru.blaga@plexus.tech>
2025-01-29 11:38:32 +0100

    Initial Commit

---
+++ ProofOfConcept/ProofOfConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProofOfConceptApp.swift
+//  ProofOfConcept
+//
+//  Created by Alexandru Robert Blaga on 29/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ProofOfConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProofOfConceptTests/ProofOfConceptTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProofOfConceptTests.swift
+//  ProofOfConceptTests
+//
+//  Created by Alexandru Robert Blaga on 29/1/25.
+//
+
+import Testing
+@testable import ProofOfConcept
+
+struct ProofOfConceptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
