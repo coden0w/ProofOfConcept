52fbaa413fe32d6837badc1c2a27edbc371bd072
davilinho <davilinho@gmail.com>
2025-03-04 13:21:13 +0100

    Code review

--- AnyFileViewer/Sources/AnyFileViewer/Models/AnyFileModel.swift
+++ AnyFileViewer/Sources/AnyFileViewer/Models/AnyFileModel.swift
@@ -7,10 +7,20 @@
 
 public struct AnyFileModel {
     public let url: String
-    public let ext: String
+    public let ext: FileExtensionType
     
-    public init(url: String, ext: String) {
+    public init(url: String, ext: FileExtensionType) {
         self.url = url
         self.ext = ext
     }
+}
+
+public enum FileExtensionType: String {
+    case pdf
+    case img
+    case xls
+    case doc
+    case zip
+    case mp4
+    case txt
 }
